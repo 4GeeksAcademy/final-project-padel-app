575c1a08a9538fcbc1c3cd37307ea8b38859f4ef
Liliana Gonzales Chavez <liligh14@gmail.com>
2025-12-14 16:56:17 +0000

    agregando geocalizacion

--- src/front/components/MapCard.jsx
+++ src/front/components/MapCard.jsx
@@ -1,15 +1,41 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const MapCard = () => {
+
+    useEffect(() => {
+        getLocation();
+    }, [])
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    const map = L.map("map").setView([latitude, longitude], 15);
+                    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                    }).addTo(map);
+
+                    L.marker([latitude, longitude]).addTo(map).bindPopup("Estás aquí").openPopup();
+                },
+
+                (error) => {
+                    console.error("Error obteniendo ubicación:", error);
+                }
+            );
+        } else {
+            alert("Geolocalización no soportada por tu navegador");
+        }
+    };
+
     return (
         <div className="card p-3 shadow-sm mb-4">
             <h5 className="fw-bold mb-3">Mapa</h5>
+            <div id="map" style={{ height: "400px", width: "100%" }}>
 
-            <img
-                src="https://cdn-icons-png.flaticon.com/512/854/854878.png"
-                alt="map"
-                className="img-fluid rounded"
-            />
+            </div>
 
             <button className="btn btn-light border mt-2 w-100">Ver ruta</button>
         </div>
