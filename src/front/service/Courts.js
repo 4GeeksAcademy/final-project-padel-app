caefff56e751468323950eacb25cf6a1b0557d98
AnaJaneMM <94802408+AnaJaneMM@users.noreply.github.com>
2025-12-16 15:38:18 +0000

    AYMAMASITA

--- src/front/service/Courts.js
+++ src/front/service/Courts.js
@@ -1,6 +1,6 @@
 const API_URL = import.meta.env.VITE_BACKEND_URL;
+
 export const getListCours = async() =>{
-    
     try {
         const request = await fetch(`${API_URL}/api/courts`,{
            method: "GET",
@@ -14,7 +14,23 @@ export const getListCours = async() =>{
         return response
     } catch (error) {
         console.log(error);
-        
     }
 }
 
+export const fetchNearbyCourts = async (latitude, longitude, radius = 2000) => {
+    try {
+        const request = await fetch(`${API_URL}/api/courts/fetch-nearby`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ latitude, longitude, radius })
+        })
+        const response = await request.json();
+        console.log("Nearby courts fetched:", response);
+        return response.courts;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
